028427c30dfad40ae0bb72c81d416e5028bccc87
joseph <joseph@josephjuma.com>
2024-04-11 21:19:43 -0700

    Version 1.0.1: Delete templates directory.

--- templates/generate_3D_ops.sh
+++
@@ -1,4 +0,0 @@
-cg vector3D_operators.tmp add.vars vector3d_add.h
-cg vector3D_operators.tmp sub.vars vector3d_sub.h
-cg vector3D_operators.tmp mul.vars vector3d_mul.h
-cg vector3D_operators.tmp div.vars vector3d_div.h
--- templates/generate_4D_ops.sh
+++
@@ -1,4 +0,0 @@
-cg vector4D_operators.tmp add.vars vector4d_add.h
-cg vector4D_operators.tmp sub.vars vector4d_sub.h
-cg vector4D_operators.tmp mul.vars vector4d_mul.h
-cg vector4D_operators.tmp div.vars vector4d_div.h
